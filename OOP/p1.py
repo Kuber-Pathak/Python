ab6733ba3eade72e273c4b2b860259ce9be51fb8
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-03-01 17:24:43 +0545

    modified

--- OOP/p1.py
+++ OOP/p1.py
@@ -1,12 +1,13 @@
 class Programmer:
     comapny="Microsodft"
-    def __init__(self,name.product):
+    def __init__(self,name,product):
         self.name=name
-        sefl.product=product
-    def getInfto(self)
+        self.product=product
+
+    def getInfto(self):
         print(f"The name of programemr is {self.name} and the product is {self.product}")
 
 harry = Programmer("Harry","skype")
-alka=programmer("Alka","Git")
+alka=Programmer("Alka","Git")
 harry.getInfto()
 alka.getInfto()
