0e2b0153fcc0ce28a3fa6b2872e597c2472cceab
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-03-09 20:43:04 +0545

    modified

---
+++ Inheritance/multilevel inheritance.py
@@ -0,0 +1,15 @@
+class Employee:
+    company_name= " visa "
+
+class Freelancer:
+    company = "Fiverr"
+
+    def display(self):
+        print(self.company)
+
+class Programmer(Employee,Freelancer):
+    name="Ram"
+
+p=Programmer()
+print(p.company_name)
+p.display()
--- Inheritance/single inheritance.py
+++ Inheritance/single inheritance.py
@@ -6,17 +6,13 @@ class Employee:
 
 class Programmer(Employee):
     language = "Python"
-    # company = "Youtube"
 
     def getLanguage(self):
         print(f"The language is {self.language}")
     
-    def showDetails(self):
-        print("This is an programmer")
 
 
 e = Employee()
 e.showDetails()
 p = Programmer()
 p.showDetails()
-print(p.company)
